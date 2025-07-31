2fc1aab36f945b45c5cb834bc67fc3d2a3dd5f37
Nickolas Maciel <jonaskakashi13@gmail.com>
2025-07-30 21:49:39 -0300

    JAVA2 AULA 02

---
+++ Eclipse-Workspace/n25_java2_01/src/br/com/senaisp/bauru/secao02/aula03/ArtAscii02.java
@@ -0,0 +1,24 @@
+package br.com.senaisp.bauru.secao02.aula03;
+
+public class ArtAscii02 {
+
+	public static void main(String[] args) {
+		System.out.println(" .'/,-Y\"     \"~-.");
+		System.out.println(" l.Y             ^.");
+		System.out.println(" /\\               _\\_");
+		System.out.println("i            ___/\"   \"\\");
+		System.out.println("|          /\"   \"\\   o !");
+		System.out.println("l         ]     o !__./");
+		System.out.println(" \\ _  _    \\.___./    \"~\\");
+		System.out.println("  X \\/ \\            ___./");
+		System.out.println(" ( \\ ___.   _..--~~\"   ~`-.");
+		System.out.println("` Z,--   /               \\");
+		System.out.println(" \\__.  (   /       ______)");
+		System.out.println("      \\   l  /-----~~\" /");
+		System.out.println("       Y   \\          /");
+		System.out.println(" |    \"x______.^");
+		System.out.println(" |           \\");
+		System.out.println("     |            \\");
+	}
+
+}
