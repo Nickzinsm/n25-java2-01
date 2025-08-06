1c88eb3895dbe398da29fec12993f9f8e1de6d56
Nickolas Maciel <jonaskakashi13@gmail.com>
2025-08-05 21:47:41 -0300

    JAVA2 AULA 06

---
+++ Eclipse-Workspace/n25_java2_01/src/br/com/senaisp/bauru/secao03/aula06/Parsing01.java
@@ -0,0 +1,19 @@
+package br.com.senaisp.bauru.secao03.aula06;
+
+public class Parsing01 {
+
+	public static void main(String[] args) {
+
+		int shirtPrice = Integer.parseInt("15");
+		double taxRate = Double.parseDouble("0.05");
+		String gibbersh = "887dsnds87dsfs";
+		//Mostrando os valores
+		System.out.println("Preço da camiseta: " + shirtPrice);
+		System.out.println("Percential imposto: " + taxRate);
+		
+		//exemplos de erros de conversão abaixo!
+		System.out.println("gibberish para int :" + Integer.parseInt("0.05"));
+		System.out.println("gibberish para int :" + Integer.parseInt(gibbersh));
+	}
+
+}
