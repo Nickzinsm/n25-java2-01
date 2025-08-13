c0a5eb452cfc42642010a1862da6b68db8a68085
Nickolas Maciel <jonaskakashi13@gmail.com>
2025-08-12 21:45:57 -0300

    JAVA2 AULA 10

--- Eclipse-Workspace/n25_java2_01/src/br/com/senaisp/bauru/secao04/aula09/Forca.java
+++ Eclipse-Workspace/n25_java2_01/src/br/com/senaisp/bauru/secao04/aula09/Forca.java
@@ -1,4 +1,5 @@
 package br.com.senaisp.bauru.secao04.aula09;
+
 import java.util.Random;
 import java.util.Scanner;
 
@@ -7,9 +8,10 @@ public class Forca {
 	private String palavraSecreta;
 	private String palavraMascarada;
 	private int erros;
-	//numero aleatorio
+	// numero aleatorio
 	private Random rnd;
-	//Constructor
+
+	// Constructor
 	public Forca() {
 		rnd = new Random();
 		int linhaEscolhida = rnd.nextInt(180);
@@ -17,40 +19,62 @@ public class Forca {
 		Scanner sc = new Scanner(Forca.class.getResourceAsStream("palavras.txt"));
 		while (sc.hasNext()) {
 			String apoio = sc.nextLine();
-			if (linhaAtual==linhaEscolhida);
+			if (linhaAtual == linhaEscolhida)
+				;
 			palavraSecreta = "" + apoio;
 			palavraMascarada = "_".repeat(palavraSecreta.length());
 			break;
 		}
 		linhaAtual++;
-	
-	sc.close();
-	erros = 0;
+
+		sc.close();
+		erros = 0;
 	}
-			
-	
+
 	public String getPalavraMascarada() {
 		return palavraMascarada;
 	}
+
 	public int getErros() {
 		return erros;
 	}
+
 	@Override
 	public String toString() {
-					//123456
-		String linha = " +---+\n"+
-					  " |  " + ( erros>=2 ? "\\" : " " )+
-					  		   ( erros>=1 ? "0" : " ")+
-					  		   ( erros>=3 ? "/" : " ")+ "\n" +  //linha 2
-					  " |  " + ( erros>=4 ? " |" : " ")+ "\n" +  //linha 3
-					  " |  " + ( erros>=5 ? "/" : " ")+ " "+ 	//linha 3
-					  		   ( erros>=6 ? "\\": " ")+ "\n" +  //linha 4
-					  " |\n" + 									//linha 5
-					  "=== " + getPalavraMascarada() + "\n";	//linha 6	   
-					  
-		
+		// 123456
+		String linha = " +---+\n" + " |  " + (erros >= 2 ? "\\" : " ") + (erros >= 1 ? "0" : " ")
+				+ (erros >= 3 ? "/" : " ") + "\n" + // linha 2
+				" |  " + (erros >= 4 ? " |" : " ") + "\n" + // linha 3
+				" |  " + (erros >= 5 ? "/" : " ") + " " + // linha 3
+				(erros >= 6 ? "\\" : " ") + "\n" + // linha 4
+				" |\n" + // linha 5
+				"=== " + getPalavraMascarada() + "\n"; // linha 6
+
 		return linha;
-		
+
+	}
+
+	public void lancarLetra(char letra) {
+		if (palavraSecreta.contains(("" + letra).toLowerCase())) {
+			String apoio = "";
+			for (int i = 0; i < palavraSecreta.length(); i++) {
+				if (("" + letra).toLowerCase().equals(""+palavraSecreta.charAt(i))) {
+					apoio += palavraSecreta.charAt(i);
+				} else {
+					apoio += palavraMascarada.charAt(i);
+				}
+			}
+			palavraMascarada = "" + apoio;
+		} else {
+			erros++; // Aumenta um erro
+			System.out.println("A letra " + letra + " não está no texto!");
+		}
+
+	}
+	public boolean ehFimDeJogo() {
+		return erros>=6 || !palavraMascarada.contains("_");
+	}
+	public String getPalavraRevelada( ) {
+		return ehFimDeJogo() ? palavraSecreta : "";
 	}
 }
-
